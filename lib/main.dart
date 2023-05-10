06a81db7be44324f7fe6a37573de965139be313a
Dell <darshansakhat166@gmail.com>
2023-05-10 09:43:57 +0530

    Merge remote-tracking branch 'origin/master'

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:play_store_ios_store/Google_play_store/G_Provider_screen/G_Provider_screen.dart';
 import 'package:play_store_ios_store/Google_play_store/G_View_screen/Open_screen.dart';
@@ -14,35 +16,38 @@ import 'Google_play_store/G_View_screen/Home_screen.dart';
 void main() {
   runApp(
     Sizer(
-      builder: (context, orientation, deviceType) => Platform.isAndroid
-          ? MultiProvider(
-              providers: [
-                ChangeNotifierProvider(
-                  create: (context) => I_provider_screen(),
+      builder: (context, orientation, deviceType) => DevicePreview(
+        builder: (context) => Platform.isAndroid
+            ? MultiProvider(
+                providers: [
+                  ChangeNotifierProvider(
+                    create: (context) => I_provider_screen(),
+                  ),
+                ],
+                child: CupertinoApp(
+                  debugShowCheckedModeBanner: false,
+                  routes: {
+                    "/": (p0) => Tab_bar_screen(),
+                  },
                 ),
-              ],
-              child: CupertinoApp(
-                debugShowCheckedModeBanner: false,
-                routes: {
-                  "/": (p0) => Tab_bar_screen(),
-                },
+              )
+            : MultiProvider(
+                providers: [
+                  ChangeNotifierProvider(
+                    create: (context) => G_Provider_screen(),
+                  )
+                ],
+                child: MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  routes: {
+                    "/": (context) => Home_screen(),
+                    "open": (context) => open_screen(),
+                  },
+                  theme: ThemeData.dark(),
+                ),
               ),
-            )
-          : MultiProvider(
-              providers: [
-                ChangeNotifierProvider(
-                  create: (context) => G_Provider_screen(),
-                )
-              ],
-              child: MaterialApp(
-                debugShowCheckedModeBanner: false,
-                routes: {
-                  "/": (context) => Home_screen(),
-                  "open": (context) => open_screen(),
-                },
-                theme: ThemeData.dark(),
-              ),
-            ),
+        enabled: !kReleaseMode,
+      ),
     ),
   );
 }
